d9a561fa073d081304c881bc8375e0141f95f7a4
Akshay <akshay@localhost.domain.com>
2024-01-05 00:53:27 +0530

    update files

--- ec2/instance/main.tf
+++ ec2/instance/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "terraform-instance" {
-  ami           = "ami-02a2af70a66af6dfb"
-  instance_type = "t2.micro"
-  key_name = "new"
-  security_groups = [ "ssh" , "http"]
+  ami           = "Amazon mahcine image id/ operating system id"
+  instance_type = "type of instance"
+  key_name = "name of key"
+  security_groups = [ "security" , "groups" , "name"]
   tags = {
-    Name = "terraform-instance"
+    Name = "name of instance"
   }
 }
---
+++ ec2/instance/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "access_key of user"
+  secret_key = "secret_key of user"
+}
